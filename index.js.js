525ef192f13ad90823aaf8f532efe9d8b4b25c84
RohitVer1797 <100287997+RohitVer1797@users.noreply.github.com>
2022-03-02 15:05:20 +0530

    add files via upload

---
+++ index.js.js
@@ -0,0 +1,13 @@
+/*Javascript for Toggle Menu */
+var fa = document.getElementById("fa");
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+
+
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+
+}
